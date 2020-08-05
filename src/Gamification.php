42dedf1999ddc34d62640b67f61e50ff75816c5d
Anushan iMac <anushanw@flatorb.com>
2020-08-05 20:03:16 +0530

    Functionality improvements

--- src/Gamification.php
+++ src/Gamification.php
@@ -2,6 +2,7 @@
 
 namespace Flatorb\Gamification;
 
+use Flatorb\Gamification\Models\Achievement;
 use Flatorb\Gamification\Models\AchievementType;
 
 class Gamification
@@ -9,5 +10,10 @@ class Gamification
     public static function achievementTypes()
     {
         return AchievementType::get();
+    }
+
+    public static function getAchievements()
+    {
+        return Achievement::get();
     }
 }
--- src/Models/Achievement.php
+++ src/Models/Achievement.php
@@ -12,4 +12,9 @@ class Achievement extends Model
     protected $table = 'gamification_achievements';
 
     protected $fillable =['name', 'type_id', 'goal', 'goal_value', 'icon', 'meta'];
+
+    public function type()
+    {
+        return $this->belongsTo('Flatorb\Gamification\Models\AchievementType', 'type_id', 'id')->withDefault();
+    }
 }
